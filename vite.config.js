3a4b8547e067d04b240c3b16061983c01c15017a
MUHAMMAD DHIYAUL ATHA <mdhyaulatha@gmail.com>
2025-08-13 07:44:57 +0700

    feat: upgrade to PWA, add vite-plugin-pwa & manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // sw akan update sendiri
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+      },
+      manifest: {
+        name: 'Muslim Life',
+        short_name: 'MuslimLife',
+        description: 'Aplikasi Islami harian – Qur’an, jadwal shalat, doa & artikel',
+        theme_color: '#10b981',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
